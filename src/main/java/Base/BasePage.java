2dc1d74eafa3545b54075862a8d6c2d35deda1f2
AnnThanhh <trinhanthanh@gmail.com>
2026-05-10 17:13:36 +0700

    update buổi 26

--- src/main/java/Base/BasePage.java
+++ src/main/java/Base/BasePage.java
@@ -1,6 +1,8 @@
 package Base;
 
 import Constants.TimeOutConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,25 +14,28 @@ import java.time.Duration;
 public class BasePage {
     //thuộc tính
     protected WebDriver driver;
-
+    protected final Logger LOG = LogManager.getLogger(getClass());
     public BasePage(WebDriver webDriver){
         this.driver = webDriver;
     }
 
     //phương thức
     public WebElement waitForElementVisible(By locator, long timeOutInSec){
+        LOG.info("Waiting for element visible with locator: " + locator.toString() + " and timeout: " + timeOutInSec + " seconds");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSec));
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         return element;
     }
 
     public WebElement waitForElementClickable(By locator, long timeOutInSec){
+        LOG.info("Waiting for element clickable with locator: " + locator.toString() + " and timeout: " + timeOutInSec + " seconds");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSec));
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
         return element;
     }
 
     public void inputText(By locator, String text, long timeOutInSec){
+        LOG.info("Inputting text: '" + text + "' into element with locator: " + locator.toString() + " and timeout: " + timeOutInSec + " seconds");
         //code nhập text vào ô input
         WebElement element = waitForElementVisible(locator, timeOutInSec);
         element.sendKeys(text);
@@ -41,6 +46,7 @@ public class BasePage {
     }
 
     public void click(By locator, long timeOutInSec){
+        LOG.info("Clicking on element with locator: " + locator.toString() + " and timeout: " + timeOutInSec + " seconds");
         WebElement btn = waitForElementClickable(locator, timeOutInSec);
         btn.click();
     }
@@ -49,6 +55,7 @@ public class BasePage {
     }
 
     public String getText(By locator, long timeOutInSect){
+        LOG.info("Getting text from element with locator: " + locator.toString() + " and timeout: " + timeOutInSect + " seconds");
         WebElement element = waitForElementVisible(locator, timeOutInSect);
         return element.getText();
     }
--- src/test/java/testcases/register/Register01_Verify_Valid_Register.java
+++ src/test/java/testcases/register/Register01_Verify_Valid_Register.java
@@ -1,6 +1,7 @@
 package testcases.register;
 
 import Base.BaseTest;
+import Report.ExtentReportManager;
 import drivers.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -28,6 +29,8 @@ public class Register01_Verify_Valid_Register extends BaseTest {
     public void test_valid_register() {
         WebDriver driver = DriverFactory.getDriver();
         driver.manage().window().maximize();
+        ExtentReportManager.info("bước 1: mở trình duyệt và truy cập và trang Demo1");
+        LOG.info("bước 1: mở trình duyệt và truy cập và trang Demo1");
         driver.get("https://demo1.cybersoft.edu.vn"); // home page
         //khai bao WebDriverWait
 //        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(30));
@@ -45,60 +48,89 @@ public class Register01_Verify_Valid_Register extends BaseTest {
 
         //Pre-condition: User is on Register page
         //Click on 'Đăng Ký' link on the top right
+        ExtentReportManager.info("bước 2: click vào link 'Đăng Ký' trên thanh điều hướng");
+        LOG.info("bước 2: click vào link 'Đăng Ký' trên thanh điều hướng");
         homePage.getTopBarNavigation().navigateToRegisterPage();
-
+        ExtentReportManager.info("bước 3: nhập thông tin hợp vào form đăng ký");
+        LOG.info("bước 3: nhập thông tin hợp vào form đăng ký");
         //Step 1: Enter account on 'Tài Khoản' textbox
         String accountName = UUID.randomUUID().toString(); // 32 ky tu
         String email = accountName + "@example.com";
         System.out.println("Account: " + accountName);
         System.out.println("Email: " + email);
 
+        ExtentReportManager.info("bước 4: nhập thông tin hợp vào ô tài khoản");
+        LOG.info("bước 4: nhập thông tin hợp vào ô tài khoản");
         //khai bao locator kieu By (chưa tìm element)
         registerPage.enterAccount(accountName);
 
+        ExtentReportManager.info("bước 5: nhập thông tin hợp vào ô mật khẩu");
+        LOG.info("bước 5: nhập thông tin hợp vào ô mật khẩu");
         //Step 2: Enter password
         String password = "123456";
         registerPage.enterPassword(password);
 
+        ExtentReportManager.info("bước 6: nhập thông tin hợp vào ô nhập lại mật khẩu");
+        LOG.info("bước 6: nhập thông tin hợp vào ô nhập lại mật khẩu");
         //Step 3: Re-enter password
         registerPage.reEnterPassword(password);
 
+        ExtentReportManager.info("bước 7: nhập thông tin hợp vào ô nhập lại họ tên");
+        LOG.info("bước 7: nhập thông tin hợp vào ô nhập lại họ tên");
         //Step 4: Enter fullname
         registerPage.enterFullName("John Johnson");
 
+        ExtentReportManager.info("bước 8: nhập thông tin hợp vào ô nhập lại email");
+        LOG.info("bước 8: nhập thông tin hợp vào ô nhập lại email");
         //Step 5: Enter email
         registerPage.enterEmail(email);
 
+        ExtentReportManager.info("bước 9: click vào button 'Đăng Ký'");
+        LOG.info("bước 9: click vào button 'Đăng Ký'");
         //Step 6: Click register
         registerPage.clickRegister();
 
         //Step 7: Verify valid register successfully
         //VP1 (Verify Point 1): 'Đăng Ký thành công' message displays
+        ExtentReportManager.info("bước 10: kiểm tra message 'Đăng Ký thành công' hiển thị");
+        LOG.info("bước 10: kiểm tra message 'Đăng Ký thành công' hiển thị");
         CommonDialog commonDialog = new CommonDialog(driver);
         String recordedRegisterMsg = commonDialog.getMsg();
         Assert.assertEquals(recordedRegisterMsg, "Đăng ký thành công", "Register text message failed !");
 
         //Wait message dialog disappear
 //        commonDialog.waitForDialogDisappear();
+        ExtentReportManager.info("bước 11: click vào button 'Đóng' trên message dialog");
+        LOG.info("bước 11: click vào button 'Đóng' trên message dialog");
         //Click 'Đồng ý' button on message dialog
         commonDialog.clickBtnAgree();
 
         //VP2: Login successfully with registered account
+        ExtentReportManager.info("bước 12: click vào link 'Đăng Nhập' trên thanh điều hướng");
+        LOG.info("bước 12: click vào link 'Đăng Nhập' trên thanh điều hướng");
         //Click 'Dang Nhap' link
         registerPage.getTopBarNavigation().navigateToLoginPage();
 
+        ExtentReportManager.info("bước 13: nhập thông tin hợp lệ vào ô tài khoản");
+        LOG.info("bước 13: nhập thông tin hợp lệ vào ô tài khoản");
         //Enter account to login
         loginPage.enterAccount(accountName);
 
+        ExtentReportManager.info("bước 14: nhập thông tin hợp lệ vào ô mật khẩu");
+        LOG.info("bước 14: nhập thông tin hợp lệ vào ô mật khẩu");
         //Enter password to login
         loginPage.enterPassword(password);
 
+        ExtentReportManager.info("bước 15: click vào button 'Đăng Nhập'");
+        LOG.info("bước 15: click vào button 'Đăng Nhập'");
         //Click 'Dang Nhap' button
         loginPage.clickLogin();
 
         //VP: 'Đăng nhập thành công' message displays
+        ExtentReportManager.info("bước 16: kiểm tra message 'Đăng Nhập thành công' hiển thị");
+        LOG.info("bước 16: kiểm tra message 'Đăng Nhập thành công' hiển thị");
         String recordedLoginMsg = commonDialog.getMsg();
-        Assert.assertEquals(recordedLoginMsg, "Đăng nhập thành công", "Login text message failed !");
+        Assert.assertEquals(recordedLoginMsg, "Đăng nhập thành công!", "Login text message failed !");
 
 //        driver.quit(); // Close browser & Quit chromedriver process
     }
